a3deb9daac3435d274f790c7e4177ea80c3648ba
Juan Ignacio Cherrutti <juancherru@gmail.com>
2022-09-13 10:06:50 -0300

    Nullable

--- MusicStoreMVC/Controllers/DemoController.cs
+++ MusicStoreMVC/Controllers/DemoController.cs
@@ -1,7 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+
 namespace MusicStoreMVC.Controllers
 {
 public class DemoController : Controller
 {
+    public IActionResult Index()
+    {
+        return View();
+    }
 
 }
 
